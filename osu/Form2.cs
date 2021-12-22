f581adbad3af420bbaff2ad1eca6d19dc32c1f08
rhythmxqq <96514857+rhythmxqq@users.noreply.github.com>
2021-12-22 15:31:41 +0300

    zxc
    zxc

---
+++ osu/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace osu
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private SoundPlayer sp;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(Resource1.chel);
+            player.Play();
+            if (textBox1.Text == string.Empty)
+            {
+                
+                MessageBox.Show("Введите никнейм");
+                return;
+            }
+           
+            Hide();
+            Form1 f = new Form1();
+            f.ShowDialog();
+            this.Close();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
